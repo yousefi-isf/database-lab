6343f56e00dc288b4bdf4ae86346b3ecf36868d1
Mohamad Yousefi <91023226+yousefi-isf@users.noreply.github.com>
2023-10-09 21:46:30 +0330

    insert data

---
+++ tamarin/s2/queries.sql
@@ -0,0 +1,15 @@
+-- Insert data to [messages] table
+INSERT INTO `messages`(
+    `title`,
+    `email`,
+    `description`,
+    `created_at`,
+    `replied_at`
+)
+VALUES(
+    'مشکل در احراز هویت ، بررسی شود',
+    NULL,
+    NULL,
+    NULL,
+    NULL
+);
